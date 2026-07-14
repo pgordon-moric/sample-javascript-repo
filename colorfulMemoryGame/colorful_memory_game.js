b1beffb0808553b39a2c8646d8caa7de2473740e
pgordon-moric <pgordon@moric.org>
2026-07-14 16:37:40 -0400

    added generateCards function

---
+++ colorfulMemoryGame/colorful_memory_game.js
@@ -0,0 +1,21 @@
+const colors = ['red', 'blue', 'green', 'purple', 'orange', 'pink', 'red', 'blue', 'green', 'purple', 'orange', 'pink'];
+let cards = shuffle(colors.concat(colors));
+let selectedCards = [];
+let score = 0;
+let timeLeft = 30;
+let gameInterval;
+
+const startbtn = document.getElementById('startbtn');
+const gameContainer = document.getElementById('game-container');
+const scoreElement = document.getElementById('score');
+const timerElement = document.getElementById('timer');
+
+function generateCards(){
+    for (const color of cards){
+        const card = document.createElement('div');
+        card.classList.add('card');
+        card.dataset.color = color;
+        card.textContent = '?';
+        gameContainer.appendChild(card);
+    }
+}
