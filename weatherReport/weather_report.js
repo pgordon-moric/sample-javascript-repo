5738424355fedbdfaa9b342ae27b080496e56637
pgordon-moric <pgordon@moric.org>
2026-07-17 15:28:21 -0400

    built weather lookup tool

---
+++ weatherReport/weather_report.js
@@ -0,0 +1,23 @@
+function showweatherDetails(event){
+    event.preventDefault();
+
+    const city = document.getElementById('city').value;
+    const apiKey = '745eda45e72939c7e202e1d3071d6c73';
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            const weatherInfo = document.getElementById('weatherInfo');
+            weatherInfo.innerHTML = `<h2>Weather in ${data.name}</h2>
+                                    <p>Temperature: ${data.main.temp} &#8451;</p>
+                                    <p>Weather: ${data.weather[0].description}</p>`;
+        })
+        .catch(error => {
+            console.error('Error fetching weather:', error);
+            const weatherInfo = document.getElementById('weatherInfo');
+            weatherInfo.innerHTML = `<p>Failed to fetch weather. Please try again.</p>`;
+        });
+}
+
+document.getElementById('weatherForm').addEventListener('submit',showweatherDetails);
